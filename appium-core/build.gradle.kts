97d66bde65d50df3c6c7ccae11da3d200b63179c
Ivan Zdanovich <i.zdanovich@godeltech.com>
2024-06-06 10:30:04 +0200

    Configured gradle multi project

--- appium-core/build.gradle.kts
+++ appium-core/build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     kotlin("jvm")
 }
+
+// Appium - Selenium compatibility matrix: https://github.com/appium/java-client?tab=readme-ov-file#compatibility-matrix
+val appiumVersion = "9.2.3"
+val seleniumVersion = "4.19.0"
+
+dependencies {
+    implementation("io.appium:java-client:${appiumVersion}")
+    implementation("org.seleniumhq.selenium:selenium-java:${seleniumVersion}")
+}
--- appium-test/build.gradle.kts
+++ appium-test/build.gradle.kts
@@ -2,5 +2,8 @@ plugins {
     kotlin("jvm")
 }
 
+val testNGVersion = "7.10.2"
+
 dependencies {
+    testImplementation("org.testng:testng:${testNGVersion}")
 }
